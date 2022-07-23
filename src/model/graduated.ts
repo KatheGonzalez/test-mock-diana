11d192f4c6520c0a6d4da3fa8d9f952632e28c23
KatheGonzalez <99217114+KatheGonzalez@users.noreply.github.com>
2022-07-23 18:55:06 -0500

    add email

--- src/model/graduated.ts
+++ src/model/graduated.ts
@@ -1,11 +1,13 @@
 import { InMemoryDBEntity } from '@nestjs-addons/in-memory-db';
 
 export interface Graduated extends InMemoryDBEntity {
+  id : string;
   dni: number;
   cellphone: number;
   first_name: string;
   second_name: string;
   last_name: string;
   second_last_name: string;
+  email: string;
   photo: string;
 }
